c564ecb36d2cbe56f484d55a122652591aa28182
Vasanth KG <vasanth@egovernments.org>
2017-11-13 15:28:48 +0530

    Minor changes

--- Model/seed-job.groovy
+++ Model/seed-job.groovy
@@ -1,8 +1,23 @@
-def createSeedJob(def moduleName)
+def createSeedJob(moduleName,serviceName,gitRepoUrl)
 {
 	stage("Create-Seed-Job")
 	{
-			sh "echo ${moduleName}"
+		// Folder Create If not exists
+		jenkins = Jenkins.instance
+		def folder = jenkins.getItemByFullName(moduleName)
+		if (folder == null) {
+		  println "Creating Folder -- ${moduleName} ..!!"
+		  def jenkinsInstance = Jenkins.getInstance()
+		  folder = jenkinsInstance.createProject(Folder.class, moduleName)
+		}
+		// Job Create
+		def scm = new GitSCM(gitRepoUrl)
+		scm.branches = [new BranchSpec("*/master")];
+		def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
+		def parent = Jenkins.instance
+		def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, JOB_NAME)
+		job.definition = flowDefinition
+		parent.reload()
 	}
 }
 
@@ -22,4 +37,12 @@ def publishGithub(def moduleName)
 	}
 }
 
+def defaultServiceOverrides = { folder, jobName ->
+    return {
+      create = ["uri": "/jenkins/job/${folder}/createItem", params: [ name: "${jobName}" ] ]
+      update = ["uri": "/jenkins/job/${folder}/job/${jobName}/config.xml"]
+      get = ["uri": "/jenkins/job/${folder}/job/${jobName}/config.xml"]
+      delete = ["uri": "/jenkins/job/${folder}/job/${jobName}/doDelete"]
+    }
+}
 return this;
