4bf6433ae5b0da252a938a2377ccb97a99bae38a
Vasanth KG <vasanth@egovernments.org>
2017-11-13 13:43:27 +0530

    Systan error fix

--- Model/seed-job.groovy
+++ Model/seed-job.groovy
@@ -1,16 +1,16 @@
 def createSeedJob (moduleName) {
 	stage '\u278B Create Seed Job'
-	println ${moduleName}
+	println "${moduleName}"
 }
 
 def createDockerTemplate (moduleName) {
 	stage '\u278C Create Pipeline Template'
-	println ${moduleName}
+	println "${moduleName}"
 }
 
 def publish(moduleName) {
 	stage '\u278D Publish to SCM'
-	println ${moduleName}
+	println "${moduleName}"
 }
 
 return this;
